2929ca441791c7939c35d5962a2c59d348118091
Brad Gardner <bradgardner@sevenhillstechnology.com>
2017-10-13 09:48:37 -0400

    updated to use promises

--- send.js
+++ send.js
@@ -2,6 +2,19 @@ const zfs_api = require('./zfs_api');
 
 const api = new zfs_api();
 
-console.log('creating snapshot');
-api.create_snapshot('dev1@dev1snap1');
-console.log('finished creating snapshot');
+
+async function execute() {
+	try {
+		console.log('creating snapshot');
+
+		await api.create_snapshot('dev1@dev1snap1');
+		
+		console.log('finished creating snapshot');
+	} catch(err) {
+		console.log('failed to create snapshot');
+		console.log(err);
+	}
+};
+
+execute();
+
--- zfs_api.js
+++ zfs_api.js
@@ -10,62 +10,94 @@ class ZFSApi {
 	}
 
 	create_snapshot(snapshot_name) {
-		console.log('spawning zfs snapshot');
+		const promise = new Promise((resolve, reject) => {
+			console.log('spawning zfs snapshot');
 
-		var child = spawn('sh', ['-c', `zfs snapshot ${snapshot_name}`]);
+			var child = spawn('sh', ['-c', `zfs snapshot ${snapshot_name}`]);
 
-		child.addListener('exit', function(code){
-			console.log(`Snapshot create complete: ${code}`);
+			child.addListener('exit', function(code){
+				console.log(`Snapshot create complete: ${code}`);
 
-			return code || 0;
+				if (code === 0) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
 		});
+		
+		return promise;
 	}
 
 	send_file(snapshot_name, file_name) {
-		console.log('spawning zfs send to file');
+		const promise = new Promise((resolve, reject) => {
+			console.log('spawning zfs send to file');
 
-		var child = spawn('sh', ['-c', `zfs send ${snapshot_name} > ${file_name}`]);
-		
-		child.addListener('exit', function (code) {
-			console.log(`Send complete: ${code}`);
+			var child = spawn('sh', ['-c', `zfs send ${snapshot_name} > ${file_name}`]);
 			
-			return code || 0;
+			child.addListener('exit', function (code) {
+				console.log(`Send complete: ${code}`);
+				
+				if (code === 0) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
 		});
 	}
 
 	send_mbuffer(snapshot_name, host, port) {
-		console.log('spawning zfs send to mbuffer');
+		const promise = new Promise((resolve, reject) => {
+			console.log('spawning zfs send to mbuffer');
 
-		var child = spawn('sh', ['-c', `zfs send ${snapshot_name} | mbuffer -O ${host}:${port}`]);
+			var child = spawn('sh', ['-c', `zfs send ${snapshot_name} | mbuffer -O ${host}:${port}`]);
 
-		child.addListener('exit', function (code) {
-			console.log(`Send complete: ${code}`);
+			child.addListener('exit', function (code) {
+				console.log(`Send complete: ${code}`);
 
-			return code || 0;
+				if (code === 0) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
 		});
 	}
 
 	receive_mbuffer_to_file(file_name, port) {
-		console.log('spwaning mbuffer to receive to file');
+		const promise = new Promise((resolve, reject) => {
+			console.log('spwaning mbuffer to receive to file');
 
-		var child = spawn('sh', ['-c', `mbuffer -I ${port} -o ${file_name}`]);
+			var child = spawn('sh', ['-c', `mbuffer -I ${port} -o ${file_name}`]);
 
-		child.addListener('exit', function(code){
-			console.log(`Receive complete: ${code}`);
-			
-			return code || 0;
+			child.addListener('exit', function(code){
+				console.log(`Receive complete: ${code}`);
+				
+				if (code === 0) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
 		});
 	}
 
 	receive_mbuffer_to_zfs_receive(receive_target, port) {
-		console.log('spawning mbuffer to receive to zfs receive');
+		const promise = new Promise((resolve, reject) => {
+			console.log('spawning mbuffer to receive to zfs receive');
 
-		var child = spawn('sh', ['-c', `mbuffer -I ${port} | zfs receive ${receive_target}`]);
+			var child = spawn('sh', ['-c', `mbuffer -I ${port} | zfs receive ${receive_target}`]);
 
-		child.addListener('exit', function(code){
-			console.log(`Receive complete: ${code}`);
+			child.addListener('exit', function(code){
+				console.log(`Receive complete: ${code}`);
 
-			return code || 0;
+				if (code === 0) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
 		});
 	}
 }
